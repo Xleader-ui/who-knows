437da50860a3fc8aaa14bed903e3e02ac21e2b07
Xleader-ui <nolanrmcanally@gmail.com>
2026-08-24 13:59:44 +0500

    Delete pocket-arcade directory

--- pocket-arcade/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.stardodger.game',
-  appName: 'Star Dodger',
-  webDir: 'www'
-};
-
-export default config;
